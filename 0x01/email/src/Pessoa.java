f297b72445da2ae63c75ca2d620ea0e48315c320
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-06-08 11:16:56 -0300

    Add files via upload

---
+++ 0x01/email/src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    public static boolean emailValid(String email){
+        if(email.length() <= 50 && email.contains("@")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
